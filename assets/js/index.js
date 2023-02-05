9b2bf227e3b33d0d3b237fc40b9954e0ed635f15
samuel_tuki <smauel_tuki@yahoo.com>
2023-02-05 01:14:14 +0000

    html update

--- assets/js/index.js
+++ assets/js/index.js
@@ -9,33 +9,34 @@ $(document).ready(function () {
   //      let inputField = $("#search-input");
   //      let input = inputField.value;
   //  console.log(input);
-//   let queryUrl1 =
-//     "http://api.openweathermap.org/data/2.5/weather?q=" +
-//     input +
-//     "&cnt=10&units=imperial&APPID=" +
-//     apiKey;
+  //   let queryUrl1 =
+  //     "http://api.openweathermap.org/data/2.5/weather?q=" +
+  //     input +
+  //     "&cnt=10&units=imperial&APPID=" +
+  //     apiKey;
 
   let queryUrl2 =
     "http://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=ad466c2bde80cc851c862d4fa5cea60c";
 
-  $("#search-button").click(function () {
-    let inputField = $("#search-input").val();
+    $("#search-button").click(function () {
+        console.log("Searched Clicked")
+        let inputField = $("#search-input").val();
+        console.log(inputField);
     
-    // console.log(inputField);
-
-
-    let queryUrl1 =
-    "http://api.openweathermap.org/data/2.5/weather?q=" +
-    inputField +
-    "&cnt=10&units=imperial&APPID=" +
-    apiKey;
-
-
-    $.ajax({
-      url: queryUrl1,
-      method: "GET",
-    }).then(function (response) {
-      console.log(response);
-    });
-  });
+    
+        let queryUrl1 =
+        "http://api.openweathermap.org/data/2.5/weather?q=" +
+        inputField +
+        "&cnt=10&units=imperial&APPID=" +
+        apiKey;
+    
+    
+        $.ajax({
+          url: queryUrl1,
+          method: "GET",
+        }).then(function (response) {
+          console.log(response);
+        });
+      });
+    
 });
