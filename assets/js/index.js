4daa3ce5a2b546d5b51c2d20c09fc004d25aee6d
samuel_tuki <smauel_tuki@yahoo.com>
2023-02-06 08:08:47 +0000

    appending current weather on page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,42 +1,52 @@
-$(document).ready(function () {
-  // let currentTime = moment("#city").format()
-  let city = $("#search-input").val();
-  let tempEl = $("#temperature");
-  let cityEl = $("#city");
-  let dateEl = $("#date");
-  let iconEl = $("#weather-icon");
+// $(document).ready(function () {
+
+// });
+let fiveDayEl = $(".fiveDay");
+let city = $("#search-input").val();
+let tempEl = $("#temperature");
+let cityEl = $("#city");
+let dateEl = $("#date");
+let iconEl = $("#weather-icon");
+
+// let cardDateEl = moment().format();
+// $("#date").text(cardDateEl);
+
+
+$("#search-button").click(function (event) {
+  event.preventDefault();
+
+  let inputField = $("#search-input").val();
   let apiKey = "ad466c2bde80cc851c862d4fa5cea60c";
-  //      let inputField = $("#search-input");
-  //      let input = inputField.value;
-  //  console.log(input);
-  //   let queryUrl1 =
-  //     "http://api.openweathermap.org/data/2.5/weather?q=" +
-  //     input +
-  //     "&cnt=10&units=imperial&APPID=" +
-  //     apiKey;
+  let queryUrl1 =
+    "http://api.openweathermap.org/data/2.5/weather?q=" +
+    inputField +
+    "&cnt=10&units=imperial&APPID=" +
+    apiKey;
 
-  let queryUrl2 =
-    "http://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=ad466c2bde80cc851c862d4fa5cea60c";
+  //  call to retrieve current weather information
+  $.ajax({
+    url: queryUrl1,
+    method: "GET",
+  }).then(function (response){
+     console.log(response);
 
-    $("#search-button").click(function () {
-        console.log("Searched Clicked")
-        let inputField = $("#search-input").val();
-        console.log(inputField);
-    
-    
-        let queryUrl1 =
-        "http://api.openweathermap.org/data/2.5/weather?q=" +
-        inputField +
-        "&cnt=10&units=imperial&APPID=" +
-        apiKey;
-    
-    
-        $.ajax({
-          url: queryUrl1,
-          method: "GET",
-        }).then(function (response) {
-          console.log(response);
-        });
-      });
-    
+  $("#city").text(response.name);
+  $("#date").text("(" + moment().format("l") + ")")
+
+
+  
+
+  });
+
+
 });
+
+// let queryUrl2 =
+//   "http://api.openweathermap.org/data/2.5/forecast?lat=51.5085&lon=-0.1257&appid=ad466c2bde80cc851c862d4fa5cea60c";
+
+// call to retrieve 5days forecast
+
+// $.ajax({
+//   url: queryUrl2,
+//   method: "GET",
+// });
